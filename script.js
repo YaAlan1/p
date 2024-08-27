6b0e75c0c58ea7fcc48a3d8365ebb21f10707ff5
YaAlan1 <chill.alanus@gmail.com>
2024-08-27 09:15:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+
+let total = 0
+
+total += 1
+total += 2
+total += 3
+total += 4
+total += 5
+total += 6
+total += 7
+total += 8
+total += 9
+
+console.log(total); 
